e527548c62dabe8f9b6f645f37f89dd5023be4ee
hungphi98 <nguyenhungphi2698@Gmail.com>
2019-02-13 03:11:03 -0500

    almost there

--- routes/label.js
+++ routes/label.js
@@ -2,7 +2,14 @@ var express = require('express');
 var router = express.Router();
 var mongoose = require('mongoose');
 var app = express();
-var username = "";
+var username = "",
+    corpus = "",
+    num_id = "";
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
 const sentenceSchema = new mongoose.Schema({
     corpus: String,
     num_id: Number,
@@ -19,15 +26,49 @@ let Sentence = mongoose.model(
     'sentence',
      sentenceSchema
 );
+
+const userSchema = new mongoose.Schema({
+    username: String,
+    labels : [{
+        corpus: String,
+        sent_id: []
+    }]
+});
+let User = mongoose.model(
+    'label',
+    userSchema
+);
 app.set('view engine', 'ejs');
 
 router.get("/", (req,res)=>{
     if (req.isAuthenticated()){
         username = req.user.username;
-        Sentence.find({"num_id" : 1}, function(err,result){
+        User.find({"username": username, "labels": {$elemMatch :{"corpus": corpus}}},
+        function(err,data){
             if (err) console.log(err);
-            else if (result.length == 0) res.send("<h1>Nothing</h1>");
-            else res.send("<h1>"+result[0]["sentence"] + result[0]["corpus"] +"</h1>");
+            else if (data.length == 0){
+                User.update({"username": username},
+                    {$push : {"labels" : {"corpus": corpus, "sent_id": [1]}}});
+                num_id = 1;
+            }else{
+                let indArr = data[0]["labels"][0]["sent_id"];
+                num_id = indArr[indArr.length-1] + 1;
+            }
+        });
+        Sentence.find({"num_id": num_id, "corpus": corpus}, function(err,data){
+            if (err) console.log(err);
+            else if (data.length == 0) res.redirect("/home");
+            else{
+                res.render("label_session",{
+                    sentence_content: data[0]["sentence"],
+                    num_id: num_id,
+                    base: data[0]["base"],
+                    target: data[0]["target"],
+                    agree: 0,
+                    disagree: 0,
+                    percentage: 0
+                });
+            }
         });
     }else{
         res.redirect("/home");
@@ -35,7 +76,8 @@ router.get("/", (req,res)=>{
 
 });
 router.post("/", (req,res)=>{
-
+    corpus = req.body.corpora;
+    res.redirect("/label");
 });
 
 module.exports = router;
